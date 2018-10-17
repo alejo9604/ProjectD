e234eca814fe844757f682287ec845e243b66521
Warcos13 <rojasmillan.julian@gmail.com>
2018-10-17 12:41:57 -0500

    Fix StatsBase duplicated script, Added StatsBaseEditor script, Set base values, Every 2 points in FIDVAR upgrade in 1 respective hability in 1

--- Assets/Core/Scripts/Common/StatsBase.cs
+++ Assets/Core/Scripts/Common/StatsBase.cs
@@ -1,19 +1,198 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class StatsBase : MonoBehaviour {
+[ExecuteInEditMode]
+public class StatsBase : MonoBehaviour
+{
+    #region PRIVATE_FIELDS
 
-#region PRIVATE_FIELDS
-private 
-#endregion
+    private int baseHealth = 50; //Vida
+    private int baseArmor = 20; // Armadura
+    private int baseStrength; //Fuerza
+    private int baseIntelligence; //Inteligencia
+    private int baseDexterity; //Destreza
+    private int baseVersatility; //Versatilidad
+    private int baseCunning; //Astucia
+    private int baseResistance; //Resistencia
 
-#region PUBLIC_FIELDS
-#endregion
-	
-#region PRIVATE_FUCTIONS
-#endregion
+    private int baseInventoryCapacity; //Capacidad de inventario
+    private int baseMeleeDamage; //Daño cuerpo a cuerpo
+    private int baseStamina; //Stamina
+    private float baseAccuracy; //Punteria
+    private float baseSpeed; //Velocidad de movimiento
+    private float baseCritic; //Probabilidad de Golpe critico
+    private float baseBonusResources; //Probabilidad de bonus en recursos
 
-#region PUBLIC_FUNCTIONS
-#endregion
+    #endregion PRIVATE_FIELDS
+
+    #region PUBLIC_FIELDS
+
+    public int health
+    {
+        get
+        {
+            return baseHealth;
+        }
+        set
+        {
+            baseHealth = value;
+        }
+    }
+
+    public int armor
+    {
+        get
+        {
+            return baseArmor;
+        }
+        set
+        {
+            baseArmor = value;
+        }
+    }
+
+    public int strength
+    {
+        get
+        {
+            return baseStrength;
+        }
+        set
+        {
+            baseStrength = value;
+        }
+    }
+
+    public int intelligence
+    {
+        get
+        {
+            return baseIntelligence;
+        }
+        set
+        {
+            baseIntelligence = value;
+        }
+    }
+
+    public int dexterity
+    {
+        get
+        {
+            return baseDexterity;
+        }
+        set
+        {
+            baseDexterity = value;
+        }
+    }
+
+    public int versatility
+    {
+        get
+        {
+            return baseVersatility;
+        }
+        set
+        {
+            baseVersatility = value;
+        }
+    }
+
+    public int cunnning
+    {
+        get
+        {
+            return baseCunning;
+        }
+        set
+        {
+            baseCunning = value;
+        }
+    }
+
+    public int resistance
+    {
+        get
+        {
+            return baseResistance;
+        }
+        set
+        {
+            baseResistance = value;
+        }
+    }
+
+    public int invCapacity
+    {
+        get
+        {
+            return baseInventoryCapacity + (int)Mathf.CeilToInt((baseStrength + baseResistance) / 2);
+        }
+    }
+
+    public int meleeDamage
+    {
+        get
+        {
+            return baseMeleeDamage + (int)Mathf.CeilToInt(baseStrength / 2);
+        }
+    }
+
+    public int stamina
+    {
+        get
+        {
+            return baseStamina + (int)Mathf.CeilToInt(resistance / 2);
+        }
+    }
+
+    public float accuracy
+    {
+        get
+        {
+            return baseAccuracy + (int)Mathf.CeilToInt((baseDexterity + baseCunning) / 2);
+        }
+    }
+
+    public float speed
+    {
+        get
+        {
+            return baseSpeed + (int)Mathf.CeilToInt((baseDexterity + baseVersatility) / 2);
+        }
+    }
+
+    public float critic
+    {
+        get
+        {
+            return baseCritic + (int)Mathf.CeilToInt((intelligence + baseVersatility) / 2);
+        }
+    }
+
+    public float bonusResources
+    {
+        get
+        {
+            return baseBonusResources + (int)Mathf.CeilToInt((baseIntelligence + baseCunning) / 2);
+        }
+    }
+
+    #endregion PUBLIC_FIELDS
+
+    #region PUBLIC_FUNCTIONS
+
+    [ContextMenu("Set Values")]
+    public void SetBaseValues()
+    {
+        baseInventoryCapacity = 5;
+        baseMeleeDamage = 5;
+        baseStamina = 10;
+        baseAccuracy = 5;
+        baseSpeed = 5;
+        baseCritic = 3;
+        baseBonusResources = 0;
+    }
+
+    #endregion PUBLIC_FUNCTIONS
 }
---
+++ Assets/Editor/StatsBaseEditor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(StatsBase), true)]
+public class StatsBaseEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+
+        GUILayout.Space(10);
+        GUIStyle richTextStyle = new GUIStyle();
+        richTextStyle.richText = true;
+
+        StatsBase SB = target as StatsBase;
+
+        GUILayout.Label("General", EditorStyles.boldLabel);
+
+        SB.health = EditorGUILayout.IntField("Puntos de vida", SB.health);
+        SB.armor = EditorGUILayout.IntField("Armadura", SB.armor);
+
+        GUILayout.Space(10);
+        GUILayout.Label("FIDVAR", EditorStyles.boldLabel);
+
+        SB.strength = EditorGUILayout.IntField("Fuerza", SB.strength);
+        SB.intelligence = EditorGUILayout.IntField("Inteligencia", SB.intelligence);
+        SB.dexterity = EditorGUILayout.IntField("Destreza", SB.dexterity);
+        SB.versatility = EditorGUILayout.IntField("Versatilidad", SB.versatility);
+        SB.cunnning = EditorGUILayout.IntField("Astucia", SB.cunnning);
+        SB.resistance = EditorGUILayout.IntField("Resistencia", SB.resistance);
+
+        GUILayout.Space(10);
+        GUILayout.Label("Habilidades", EditorStyles.boldLabel);
+
+        GUILayout.Label("Capacidad de inventario: <b>" + SB.invCapacity.ToString() + "</b>", richTextStyle);
+        GUILayout.Label("Daño cuerpo a cuerpo: <b>" + SB.meleeDamage.ToString() + "</b>", richTextStyle);
+        GUILayout.Label("Stamina: <b>" + SB.stamina.ToString() + "</b>", richTextStyle);
+        GUILayout.Label("Punteria: <b>" + SB.accuracy.ToString() + "</b>", richTextStyle);
+        GUILayout.Label("Velocidad de movimiento: <b>" + SB.speed.ToString() + "</b>", richTextStyle);
+        GUILayout.Label("% Critico: <b>" + SB.critic.ToString() + "</b>", richTextStyle);
+        GUILayout.Label("% Bonus Recursos: <b>" + SB.bonusResources.ToString() + "</b>", richTextStyle);
+        GUILayout.Space(10);
+    }
+}
